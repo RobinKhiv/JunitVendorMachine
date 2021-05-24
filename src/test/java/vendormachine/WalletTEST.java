1acf8289d8c1e398e53966da5000c9ae277152a5
Robin Khiv <robinkhiv@gmail.com>
2021-05-24 14:34:32 -0700

    rk:added test to wallet class

--- src/test/java/vendormachine/WalletTEST.java
+++ src/test/java/vendormachine/WalletTEST.java
@@ -1,5 +1,9 @@
 package vendormachine;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 import vendormachine.users.util.Wallet;
 
 public class WalletTEST {
@@ -12,6 +16,34 @@ public class WalletTEST {
 	 * 
 	 * 		Goal: 80% code/line coverage
 	 * */
+	@Test
+	public void test1() {
+		Wallet w = new Wallet(299f);
+		Wallet w1 = new Wallet("Gucci", 299f);
+		Wallet w3 = new Wallet();
+		float expected = 399f;
+		String expectedBrand = "Gucci";
+		float overDraft = 1000f;
+		float underDraft = 100f;
+		float retrieve = 0f;
+		
+		w.addCredit(100f);
+		w1.setCredit(expected);
+		
+		assertEquals(expected, w.getAllCredit());
+		assertEquals(expected, w1.getAllCredit());
+		assertEquals(expectedBrand, w1.getBrand(expectedBrand));
+		
+		w.setBrand(expectedBrand);
+		assertEquals(expectedBrand, w.getBrand(expectedBrand));
+		retrieve = w.getCredit(overDraft);
+		
+		assertEquals(0, retrieve);
+		
+		retrieve = w.getCredit(underDraft);
+		assertEquals(retrieve, retrieve);
+		
+	}
 	
 	// Hint: 
 	// Others Tests for other classes that use Wallet may impact code coverage
